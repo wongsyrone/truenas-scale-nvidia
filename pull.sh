04eb30eda7f5d3b98801d31275339a00cc3f8834
themylogin <themylogin@gmail.com>
2023-07-10 17:38:35 +0200

    Fix kernel headers package version

--- pull.sh
+++ pull.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 kernel_version="$(apt info linux-headers-truenas-production-amd64 | awk '/Source:/ { print $2}' | sed 's/linux-//')"
+kernel_package_version="$(apt info linux-headers-truenas-production-amd64 | awk '/Version:/ { print $2}' | sed 's/linux-//')"
 version="$(apt info nvidia-kernel-dkms | awk '/Version:/ { print $2}')"
 version_short="$(echo "$version" | cut -f 1 -d '-')"
 for file in debian/changelog debian/control debian/rules
 do
     sed -i.bak "s/@KERNEL_VERSION@/$kernel_version/g" $file
+    sed -i.bak "s/@KERNEL_PACKAGE_VERSION@/$kernel_package_version/g" $file
     sed -i.bak "s/@VERSION@/$version/g" $file
     sed -i.bak "s/@VERSION_SHORT@/$version_short/g" $file
 done
